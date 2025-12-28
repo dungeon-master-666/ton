d42aa5b6945ad0cc4ca0cff848b44f7043c2e430
Marat S <dungeon666master@protonmail.com>
2025-12-28 19:49:42 +0000

    fix crash

--- validator/db/archive-manager.cpp
+++ validator/db/archive-manager.cpp
@@ -1099,8 +1099,12 @@ void ArchiveManager::try_catch_up_with_primary(td::Promise<td::Unit> promise) {
     }
     // catch up last package to sync latest blocks
     if (d == x->packages_.back()) {
-      auto actor_id = get_file_map(id).find(id)->second.file_actor_id();
-      if (actor_id.empty()) {
+      auto it = get_file_map(id).find(id);
+      if (it == get_file_map(id).end()) {
+        continue;
+      }
+      auto actor_id = it->second.file_actor_id();
+      if (!actor_id.is_alive()) {
         continue;
       }
       auto P = td::PromiseCreator::lambda([id, promise = ig.get_promise()](td::Result<td::Unit> R) mutable {
@@ -1140,8 +1144,12 @@ void ArchiveManager::try_catch_up_with_primary(td::Promise<td::Unit> promise) {
 
     // catch up last temp package to sync latest blocks
     if (d == x->temp_packages_.back()) {
-      auto actor_id = get_file_map(id).find(id)->second.file_actor_id();
-      if (actor_id.empty()) {
+      auto it = get_file_map(id).find(id);
+      if (it == get_file_map(id).end()) {
+        continue;
+      }
+      auto actor_id = it->second.file_actor_id();
+      if (!actor_id.is_alive()) {
         continue;
       }
       auto P = td::PromiseCreator::lambda([id, promise = ig.get_promise()](td::Result<td::Unit> R) mutable {
